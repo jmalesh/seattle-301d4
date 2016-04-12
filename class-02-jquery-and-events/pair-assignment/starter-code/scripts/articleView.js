21ec22c6800a457ecefc16968623a6a4656c413c
Rick Patci <rpatci@gmail.com>
2016-04-12 11:18:10 -0700

    update TODO comments

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -29,13 +29,16 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
-      //       and then show just the ones that match for the author that was selected.
-      //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      // TODO: If the select box was changed to an option that has a value, we need to:
+      //       1. Hide all the articles,
+      //       2. Show just the ones that match for the author that was selected.
+      //          Use an "attribute selector" to find those articles that match the value,
+      //          and fade them in for the reader.
 
     } else {
-      // TODO: If the select box was changed to an option that is blank, we should
-      //       show all the articles, except the one article we are using as a template.
+      // TODO: If the select box was changed to an option that is blank, we should:
+      //       1. Show all the articles,
+      //       2. Except the one article we are using as a template.
 
     }
     $('#category-filter').val('');
