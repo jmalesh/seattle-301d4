403c7a350b08b68464dc2eb7c6e985002d3a7a15
Rick Patci <rpatci@gmail.com>
2016-04-15 08:48:39 -0700

    unzip class-05 lecture code

---
+++ class-05-new-article-creation/lecture/starter-code/scripts/article.js
@@ -0,0 +1,34 @@
+var articles = [];
+
+function Article (opts) {
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
+
+Article.prototype.toHtml = function() {
+  var template = Handlebars.compile($('#article-template').text());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  this.body = marked(this.body);
+  
+  return template(this);
+};
+
+if (typeof rawData !== 'undefined') {
+  rawData.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
+
+  rawData.forEach(function(ele) {
+    articles.push(new Article(ele));
+  })
+}
+
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml())
+});
