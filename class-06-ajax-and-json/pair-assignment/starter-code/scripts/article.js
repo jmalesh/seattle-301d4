6300449ecc8b06a10f70365b6c9ce4111e128c44
Rick Patci <rpatci@gmail.com>
2016-04-18 11:02:56 -0700

    unzip starter-code for class-06

---
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
@@ -0,0 +1,76 @@
+function Article (opts) {
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
+
+// DONE: Instead of a global `articles = []` array, let's track this list of all articles directly on the
+// constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves
+// objects, which means we can add properties/values to them at any time. In this case, we have
+// a key/value pair to track, that relates to ALL of the Article objects, so it does not belong on
+// the prototype, as that would only be relevant to a single instantiated Article.
+Article.all = [];
+
+Article.prototype.toHtml = function() {
+  var template = Handlebars.compile($('#article-template').text());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  this.body = marked(this.body);
+
+  return template(this);
+};
+
+// DONE: There are some other functions that also relate to articles across the board, rather than
+// just single instances. Object-oriented programming would call these "class-level" functions,
+// that are relevant to the entire "class" of objects that are Articles.
+
+// DONE: This function will take our data, how ever it is provided,
+// and use it to instantiate all the articles. This code is moved from elsewhere, and
+// encapsulated in a simply-named function for clarity.
+Article.loadAll = function(dataPassedIn) {
+  dataPassedIn.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
+
+  dataPassedIn.forEach(function(ele) {
+    Article.all.push(new Article(ele));
+  })
+}
+
+// This function below will retrieve the data from either a local or remote source,
+// and process it, then hand off control to the View.
+Article.fetchAll = function() {
+  if (localStorage.hackerIpsum) {
+    // When our data is already in localStorage,
+    // we can load it by calling the .loadAll() method,
+    // and then render the index page (using the proper method on the articleView object).
+    Article.loadAll(//TODO: What do we pass in here to the .loadAll() method? Be careful
+      // when handling different data types between here and localStorage!
+    );
+    articleView.someFunctionToCall//(); //TODO: Change this fake method call to the correct
+    // one that will render the index page.
+  } else {
+    // TODO: When we don't already have our data, we need to:
+    // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
+
+    // 2. Store the resulting JSON data with the .loadAll method,
+
+    // 3. Cache it in localStorage so we can skip the server call next time,
+
+    // 4. And then render the index page (perhaps with an articleView method?).
+
+  }
+}
+
+/* Great work so far! STRETCH GOAL TIME! Cache the eTag located in Headers, to see if it's updated!
+  Article.fetchAll = function() {
+    if (localStorage.hackerIpsum) {
+    // Let's make a request to get the eTag (hint: you may need to use a different
+    // jQuery method for this more verbose request).
+  } else {}
+}
+*/
---
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/articleView.js
@@ -0,0 +1,113 @@
+// Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
+var articleView = {};
+
+articleView.populateFilters = function() {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      var val = $(this).find('address a').text();
+      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      $('#author-filter').append(optionTag);
+
+      val = $(this).attr('data-category');
+      optionTag = '<option value="' + val + '">' + val + '</option>';
+      if ($('#category-filter option[value="' + val + '"]').length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+articleView.handleAuthorFilter = function() {
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-author="' + $(this).val() + '"]').fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#category-filter').val('');
+  });
+};
+
+articleView.handleCategoryFilter = function() {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="' + $(this).val() + '"]').fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+articleView.handleMainNav = function() {
+  $('.main-nav').on('click', '.tab', function(e) {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+
+  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+};
+
+articleView.setTeasers = function() {
+  $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
+
+  $('#articles').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    $(this).parent().find('*').fadeIn();
+    $(this).hide();
+  });
+};
+
+articleView.initNewArticlePage = function() {
+  $('.tab-content').show();
+  $('#export-field').hide();
+  $('#article-json').on('focus', function(){
+    this.select();
+  });
+
+  $('#new-form').on('change', 'input, textarea', articleView.create);
+};
+
+articleView.create = function() {
+  var article;
+  $('#articles').empty();
+
+  // Instantiate an article based on what's in the form fields:
+  article = new Article({
+    title: $('#article-title').val(),
+    author: $('#article-author').val(),
+    authorUrl: $('#article-author-url').val(),
+    category: $('#article-category').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('#article-published:checked').length ? util.today() : null
+  });
+
+  // Use the Handblebars template to put this new article into the DOM:
+  $('#articles').append(article.toHtml());
+
+  // Activate the highlighting of any code blocks:
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+
+  // Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#export-field').show();
+  $('#article-json').val(JSON.stringify(article) + ',');
+};
+
+
+articleView.initIndexPage = function() {
+  Article.all.forEach(function(a){
+    $('#articles').append(a.toHtml())
+  });
+
+  articleView.populateFilters();
+  articleView.handleCategoryFilter();
+  articleView.handleAuthorFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
+};
