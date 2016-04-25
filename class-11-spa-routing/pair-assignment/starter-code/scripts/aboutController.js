ba3f1db876d29498dfc171460780290b78bb542a
Rick Patci <rpatci@gmail.com>
2016-04-25 09:37:20 -0700

    unzip starter-code class-11

---
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -0,0 +1,9 @@
+(function(module) {
+  var aboutController = {};
+
+  aboutController.index = function() {
+    // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  };
+
+  module.aboutController = aboutController;
+})(window);
---
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -0,0 +1,4 @@
+// TODO: Configure our two routes for this app with page.js, by registering
+//  each URL your app can handle, linked to a single controller function to handle it:
+
+// TODO: What function do we call to activate page.js? Fire it off now, to execute it:
